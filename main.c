f61eb252c0d155e6db1d7b240ade8ebd8dfd1f72
sangle <thanhsang22440011@gmail.com>
2025-07-10 22:27:14 +0700

    Update code

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -96,24 +97,195 @@ void printList(Node* head) {
     printf("NULL\n");
 }
 
+void reverseList(Node** head) {
+    Node* temp = *head;
+
+    if (temp == NULL) {
+        printf("List is empty");
+        return;
+    }
+
+    Node* prev = NULL;
+    Node* nextNode = NULL;
+
+    while (temp != NULL) {
+        nextNode = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = nextNode;
+    }
+
+    *head = prev;
+}
+
+void changeAdjacentElements(Node** head) {
+    Node* temp = *head;
+
+    if (temp == NULL) {
+        printf("List is empty");
+        return;
+    }
+
+    Node* prev = NULL;
+    Node* nextNode = NULL;
+
+    *head = temp->next;
+
+    while (temp != NULL && temp->next != NULL) {
+        Node* nextNextNode = temp->next->next;
+        nextNode = temp->next;
+        nextNode->next = temp;
+
+        if (prev != NULL) {
+            prev->next = nextNode;
+        }
+
+        prev = temp;
+        temp = nextNextNode;
+    }
+
+    if (temp != NULL) {
+        prev->next = temp;
+    } else {
+        prev->next = NULL;
+    }
+}
+
+Node* insertionSortList(Node* head) {
+    Node* currentList = head;
+
+    if (currentList == NULL) {
+        printf("List is empty");
+        return NULL;
+    }
+
+    Node* store = NULL;
+
+    while (currentList != NULL) {
+        Node* next = currentList->next;
+
+        if (store == NULL || store->data >= currentList->data) {
+            currentList->next = store;
+            store = currentList;
+        } else {
+            Node* temp = store;
+            while (temp->next != NULL && temp->next->data < currentList->data ) {
+                temp = temp->next;
+            }
+            currentList->next = temp->next;
+            temp->next = currentList;
+        }
+
+        currentList = next;
+    }
+
+    return store;
+}
+
+void bubbleSortList(Node* head) {
+    bool isSwap = false;
+
+    if (head == NULL) {
+        printf("List is empty");
+        return;
+    }
+
+    Node* fixedNode = NULL;
+
+    do {
+        isSwap = false;
+
+        Node* temp = head;
+
+        while (temp->next != fixedNode) {
+            if (temp->data > temp->next->data) {
+                int tempData = temp->data;
+                temp->data = temp->next->data;
+                temp->next->data = tempData;
+                isSwap = true;
+            }
+
+            temp = temp->next;
+        }
+        fixedNode = temp;
+    } while (isSwap);
+}
+
+Node* getMiddleNode(Node* head) {
+    if (head == NULL) {
+        printf("List is empty");
+        return head;
+    }
+
+    Node* slow = head;
+    Node* fast = head->next;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+Node* sortedMerge(Node* left, Node* right) {
+    if (left == NULL) return right;
+    if (right == NULL) return left;
+
+    Node* result = NULL;
+
+    if (left->data <= right->data) {
+        result = left;
+        result->next = sortedMerge(left->next, right);
+    } else {
+        result = right;
+        result->next = sortedMerge(left, right->next);
+    }
+
+    return result;
+}
+
+Node* mergeSortList(Node* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    Node* middleNode = getMiddleNode(head);
+    Node* nextOfMiddle = middleNode->next;
+
+    middleNode->next = NULL;
+
+    Node* left = mergeSortList(head);
+    Node* right = mergeSortList(nextOfMiddle);
+
+    return sortedMerge(left, right);
+}
+
 int main() {
     Node* head = NULL;
 
     appendNode(&head, 10);
     appendNode(&head, 14);
     appendNode(&head, 4);
+    appendNode(&head, 20);
 
     printList(head);
 
-    deleteNode(&head, 14);
+    head = mergeSortList(head);
 
     printList(head);
 
-    deleteNode(&head, 14);
-    printList(head);
+    // head = insertionSortList(head);
+    // printList(head);
 
-    insertNode(&head, 20, 1);
-    printList(head);
+    // bubbleSortList(head);
+    // printList(head);
+
+    // reverseList(&head);
+    // printList(head);
+
+    // changeAdjacentElements(&head);
+    // printList(head);
 
     return 0;
 }
